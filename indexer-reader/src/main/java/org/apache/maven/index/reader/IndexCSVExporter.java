bfeb7f914475f8353262e57675ea7dd79ed43fba
preich <preich@192.168.1.101>
2019-02-06 23:54:08 +0000

    move classes to main

--- indexer-reader/src/main/java/org/apache/maven/index/reader/IndexCSVExporter.java
+++ indexer-reader/src/main/java/org/apache/maven/index/reader/IndexCSVExporter.java
@@ -2,7 +2,6 @@ package org.apache.maven.index.reader;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,11 +13,9 @@ import java.util.stream.Collectors;
 
 import org.apache.maven.index.reader.Record.EntryKey;
 
-
 public class IndexCSVExporter {
 
 	public static void main(String[] args) {
-
 		try {
 			new IndexCSVExporter().export();
 		} catch (Exception e) {
@@ -32,34 +29,36 @@ public class IndexCSVExporter {
 		indexDir.mkdirs();
 		final File cacheDir = new File("cache/");
 		cacheDir.mkdirs();
-
-		//TODO: extract those classes from src/test into src/main or similar
+		System.out.println("Downloading..");
 		final WritableResourceHandler local = new DirectoryResourceHandler(indexDir);
 		final CachingResourceHandler remote = new CachingResourceHandler(new DirectoryResourceHandler(cacheDir),
 				new HttpResourceHandler(new URL("http://repo1.maven.org/maven2/.index/")));
 
+		
 		FileWriter fw = new FileWriter("result.txt");
 		final IndexReader indexReader = new IndexReader(local, remote);
 		List<EntryKey> keys = new ArrayList<EntryKey>();
 		Set<String> EXCLUDED_FIELDS = new HashSet<String>(Arrays.asList("description", "sha1", "name"));
 		final String delim = ";";
+		System.out.println("Processing..");
 		for (ChunkReader chunkReader : indexReader) {
 			try {
 				final RecordExpander recordExpander = new RecordExpander();
 				for (Map<String, String> rec : chunkReader) {
 					final Record record = recordExpander.apply(rec);
 					if (keys.isEmpty()) {
-						keys = record.getExpanded().keySet()
-								.stream()
-								.filter(p -> !EXCLUDED_FIELDS.contains(p.getName()))
-								.collect(Collectors.toList());
+						keys = record.getExpanded().keySet().stream()
+								.filter(p -> !EXCLUDED_FIELDS.contains(p.getName())).collect(Collectors.toList());
 						String strKey = "type;"
-								+ keys.stream().map(x -> x.getName())
-								.collect(Collectors.joining(delim));
+								+ keys.stream().map(x -> x.getName()).collect(Collectors.joining(delim));
 						fw.append(strKey).append("\n");
 					}
+					// skip zip.type=docs;zip.deployed=false;spring-ws
+					if (record.get(Record.ARTIFACT_ID) != null && record.get(Record.ARTIFACT_ID).contains(";")) {
+						continue;
+					}
 					String value = keys.stream()
-							.map(k -> record.getExpanded().getOrDefault(k, "NA").toString())
+							.map(k -> record.getExpanded().getOrDefault(k, "NA").toString().replaceAll(";", "_"))
 							.collect(Collectors.joining(delim));
 					fw.append(record.getType() + ";" + value).append("\n");
 				}
@@ -69,6 +68,7 @@ public class IndexCSVExporter {
 
 		}
 
+		System.out.println("Done..");
 		indexReader.close();
 		fw.close();
 	}
